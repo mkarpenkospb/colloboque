7de68f8a8e35f95bb45629af5999cf634a92515a
Maria Karpenko <mkarpenko.spb@mail.ru>
2020-03-20 16:22:08 +0300

    simple query to test that it works

--- client/src/main/kotlin/TestDatabase.kt
+++ client/src/main/kotlin/TestDatabase.kt
@@ -1,4 +1,8 @@
 import java.sql.DriverManager
+import java.sql.ResultSet
+
+
+
 
 fun main(args: Array<String>) {
     createFirst()
@@ -8,13 +12,22 @@ fun createFirst() {
     val url = "jdbc:h2:mem:"
     val conn = DriverManager.getConnection(url)
     val stmt = conn.createStatement()
-    val sql = "CREATE TABLE   REGISTRATION " +
-            "(id INTEGER not NULL, " +
-            " first VARCHAR(255), " +
-            " last VARCHAR(255), " +
-            " age INTEGER, " +
-            " PRIMARY KEY ( id ))"
-
+    val sql =
+     """
+         CREATE TABLE   REGISTRATION
+        (id INTEGER not NULL,
+         first VARCHAR(255),
+         last VARCHAR(255),
+         age INTEGER,
+         PRIMARY KEY ( id ))
+     """
     stmt.executeUpdate(sql)
+    stmt.executeUpdate("""
+        INSERT INTO REGISTRATION(id, first, last, age) VALUES(1, 'Maria', 'Karpenko', 26 )
+    """)
+    val res = stmt.executeQuery("SELECT * from REGISTRATION")
+    while (res.next()) {
+        println("${res.getString(1)}  ${res.getString(2)}  ${res.getString(3)}  ${res.getString(4)}")
+    }
 }
 
