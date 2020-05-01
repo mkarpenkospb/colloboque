8c855e6db80bb33dd5a952139e43363e93949d84
Maria Karpenko <mkarpenko.spb@mail.ru>
2020-05-01 10:57:46 +0300

    Prepared sql can be a field

--- client/src/main/kotlin/Log.kt
+++ client/src/main/kotlin/Log.kt
@@ -5,6 +5,9 @@ import java.sql.DriverManager
 
 class Log(private var url: String, createLogTable: String) {
 
+    val deleteSynchronized = """delete from LOG where id <= ?;"""
+    val logQuery = """INSERT INTO LOG(sql_command) VALUES ( ? );"""
+
     init {
         DriverManager.getConnection(url).use { conn ->
             conn.createStatement().use { stmt ->
@@ -16,8 +19,6 @@ class Log(private var url: String, createLogTable: String) {
 
     fun writeLog(conn: Connection, queries: List<String>) {
 
-        val logQuery = """INSERT INTO LOG(sql_command) VALUES ( ? );"""
-
         conn.prepareStatement(logQuery).use { stmt ->
             for (query in queries) {
                 stmt.setString(1, query)
@@ -28,7 +29,6 @@ class Log(private var url: String, createLogTable: String) {
     }
 
     fun clear(deleteTo: Int) {
-        val deleteSynchronized = """delete from LOG where id <= ?;"""
 
         DriverManager.getConnection(url).use { conn ->
             conn.prepareStatement(deleteSynchronized).use {stmt ->
